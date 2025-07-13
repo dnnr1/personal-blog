bc5d842d56fc03b228861d15edff70f0c55b7271
Daniel Roberto <sdanirobert@outlook.com>
2025-07-13 05:17:20 -0300

    feat: add next-theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import ThemeProvider from "./theme-provider";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-background dark:bg-dark-background text-smoth-black dark:text-white">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import ThemeSwitch from "../ThemeSwitch";
+
+function Navbar() {
+  return (
+    <div className="flex justify-around items-center p-4">
+      <h1>Hello World</h1>
+      <ThemeSwitch />
+    </div>
+  );
+}
+
+export default Navbar;
