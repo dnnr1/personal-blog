eb74a4483f6685b0a3dfdb3ea49a8f2e9c1495eb
Daniel Roberto <sdanirobert@outlook.com>
2025-12-25 22:51:47 -0300

    fix: show no content message

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,18 @@ export default async function Home() {
   return (
     <div className="w-full">
       <h1 className="text-center text-2xl underline mb-6">Latest Posts</h1>
-      <div className="grid grid-cols-2 gap-5 max-xl:grid-cols-1">
-        {posts.map((post) => (
-          <Post key={post.id} data={post} />
-        ))}
-      </div>
+
+      {posts.length === 0 ? (
+        <div className="flex items-center justify-center min-h-[60vh]">
+          <p className="text-lg">No content at the moment.</p>
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 gap-5 max-xl:grid-cols-1">
+          {posts.map((post) => (
+            <Post key={post.id} data={post} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
