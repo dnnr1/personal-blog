8ee74499a9d0d2df217270348b91224b74d5317e
Daniel Roberto <sdanirobert@outlook.com>
2026-01-20 00:28:52 -0300

    fix(formatDate): improve date formatting

--- util/formatDate.ts
+++ util/formatDate.ts
@@ -1,6 +1,11 @@
 function formatDate(dateString: string): string {
   const date = new Date(dateString);
-  return date.toLocaleDateString();
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    timeZone: "UTC",
+  }).format(date);
 }
 
 export default formatDate;
